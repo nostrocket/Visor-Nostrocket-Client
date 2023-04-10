2cc25d8f07ec526202e7f4d582a6ffff166c28bc
Kevin Gaethofs <55287702+KevinGaethofs@users.noreply.github.com>
2023-04-10 21:24:15 +0200

    No need for those extreme margins
    there is enough content to see the change with menu open

--- flamewerk/Block/Prose/ContentCenters.vue
+++ flamewerk/Block/Prose/ContentCenters.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="dark:prose-invert prose mx-auto dark:text-white mt-12 text-center max-w-7xl px-5 md:px-16 py-48 backdrop-blur-sm bg-opacity-[50%]">
+  <div class="dark:prose-invert prose mx-auto dark:text-white mt-12 text-center max-w-7xl px-5 md:px-16 backdrop-blur-sm bg-opacity-[50%]">
     <ContentSlot :use="$slots.default" unwrap="p" />
   </div>
 </template>

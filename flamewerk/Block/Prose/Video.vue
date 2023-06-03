05e20ee27846c9f079c22b5fb06825e2154a9d3f
Kevin Gaethofs <55287702+KevinGaethofs@users.noreply.github.com>
2023-06-03 14:37:24 +0200

    Empty Video Tag

--- flamewerk/Block/Prose/Video.vue
+++ flamewerk/Block/Prose/Video.vue
@@ -3,28 +3,10 @@
 
 
 
-<div class="max-w-4xl  mx-auto">
- <video width="1280" height="960" class="mx-auto my-12" autoplay muted loop>
-  <source :src="video.link" type="video/mp4">
-  <source src="/PromoVideo.ogg" type="video/ogg">
-Your browser does not support the video tag.
-</video> 
-</div>
-
-
-
-
-
 
 </template>
 
 <script setup>
-const video = defineProps({
-  link: {
-    type: String,
-    default: 'https://www.w3schools.com/html/movie.mp4'
-  }
-})
 
 
 </script>
